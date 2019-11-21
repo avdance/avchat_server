96885dbc1c18d46c6946fe561f0b0e6267891687
Guo <1055454485@qq.com>
2019-11-21 10:03:31 +0000

    <<Replace this line with your revision title>title:   update express framework
    Summary: update express framework  refer to README
    
    Test Plan: update express framework  refer to README
    
    修订人: danding, lau_yu
    
    Reviewed By: danding
    
    订阅者: Guo
    
    Diff检阅修订: http://phab.avdance.top/D17

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
-# avchat_server
-
+#avchat_server
+#************************************************
+#** last   modify  on  2019-11-21   
+#** update by      郭建勇
+#************************************************
 #install  generator:
-#sudo npm install -g express-generator
+  sudo npm install -g express-generator
+
+#taobao org
+  npm install -g  xxx  --registry=https://registry.npm.taobao.org
+#node自动重启工具nodemon
+ npm i   nodemon cross-env --save-dev   --registry=https://registry.npm.taobao.org  
+#run process
+  npm install & npm start
+  & npm run dev (加载nodemon) 
 
 #***************************************************
 #******************AVCHAT SERVER********************
 #Directory specification
-bin
-db
-publi
+avchat_server --
+ 		-bin                   运行程序
+		-conf		       全局配置文件
+		-controller            控制器(业务逻辑)
+		-db                    缓存/数据库配置
+		-logs                  运行日志存放
+		-middleware            中间件业务功能
+		-model                 数据库的常用操作及配置
+		-public		       静态资源目录 
+		-routes		       对各个路由做处理的代码逻辑
+		-utils		       全局工具类
+		-views		       类似于ejs这样的模板文件（前后端分离）
+
+部署步骤：
+1： npm  i    nodemon cross-env --save-dev   --registry=https://registry.npm.taobao.org
+2:  npm  run  dev
+
--- avchat_server/app.js
+++ avchat_server/app.js
@@ -1,36 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-/**
- * Module dependencies.
- */
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-var express = require('express')
-  , routes = require('./routes');
+var app = express();
 
-var app = module.exports = express.createServer();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-// Configuration
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+module.exports = app;
