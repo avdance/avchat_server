9207b2010d27ee05356ec13111e5a80e79aa7dde
dandingol03 <1291248710@qq.com>
2019-11-25 21:59:07 +0800

    feat:change express to support ts

--- avchat_server/src/app.ts
+++ avchat_server/src/app.ts
@@ -6,43 +6,61 @@ import logger from 'morgan';
 import { Express } from 'express-serve-static-core';
 import {IndexRoute} from './routes/index'
 import ErrorHandler from 'errorhandler'
+import {Routes} from "./routes/routes";
 import "reflect-metadata";
-import {User} from './entity/User';
 import {createConnection} from "typeorm";
 
-let app: Express = express();
+
 let router: express.Router;
 router = express.Router();
 
-// view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+createConnection().then(async connection => {
+    let app: Express = express();
 
-//路由注册
-IndexRoute.create(router);
-app.use('/', router);
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next);
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+            } else if (result !== null && result !== undefined) {
+                res.json(result);
+            }
+        });
+    });
+
+    // view engine setup
+    app.set('views', path.join(__dirname, '../views'));
+    app.set('view engine', 'jade');
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, '../public')));
 
 
-// catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
-    next(createError(404));
-});
+    // start express server
+    app.listen(3000);
 
+    // catch 404 and forward to error handler
+    app.use(function(req: Request, res: Response, next: Function) {
+        next(createError(404));
+    });
 
-// error handler
- app.use(function(err: Error, req: Request, res: Response, next: NextFunction) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(res.statusCode || 500);
-    res.render('error');
-  });
+    // error handler
+    app.use(function(err: Error, req: Request, res: Response, next: Function) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        // render the error page
+        res.status(res.statusCode || 500);
+        res.render('error');
+    });
+
+})
+
   
 
