2301dda688bbab843fed066170dfc803ee75c923
garrylea <avdance@163.com>
2019-12-03 18:05:13 +0800

    add npm install
    Summary: 1. add npm install
    
    Test Plan: none
    
    Differential Revision: http://phab.avdance.top/D45

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/node12/bin
+
+/usr/local/node12/bin/npm install
+#npm install
+
 /usr/local/node12/bin/tsc --build tsconfig.json
 #tsc --build     tsconfig.json
+
 /usr/local/node12/bin/tslint -c tslint.json 'src/**/*.ts'
 #tslint -c tslint.json 'src/**/*.ts'
 
