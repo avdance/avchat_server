c5b6f411aaf3adb0ba9348c7c6cad4a2b20c7aa8
garrylea <avdance@163.com>
2019-12-03 16:56:00 +0800

    fix build.sh bug
    Summary: remove -c option from tsc command
    
    Test Plan: none
    
    Differential Revision: http://phab.avdance.top/D44

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/node12/bin
-/usr/local/node12/bin/tsc -c --build tsconfig.json
+/usr/local/node12/bin/tsc --build tsconfig.json
 #tsc --build     tsconfig.json
 /usr/local/node12/bin/tslint -c tslint.json 'src/**/*.ts'
 #tslint -c tslint.json 'src/**/*.ts'
